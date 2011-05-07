3ddf1b73cc612e3004c5de8bc27d84a4a0d033d0
Jean V <sifo@debian>
2011-05-07 23:06:18 +0200

    [Rev 2] Création des packages

--- src/org/ig/uml/Main.java
+++ src/org/ig/uml/Main.java
@@ -1,5 +1,7 @@
 package org.ig.uml;
 
+import org.ig.uml.ui.Uml;
+
 public class Main {
 	
 	public static void main(String[]args){
---
+++ src/org/ig/uml/ui/ToolBarUML.java
@@ -0,0 +1,19 @@
+package org.ig.uml.ui;
+
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class ToolBarUML extends JToolBar {
+	
+	private static final long serialVersionUID = 7662874994619958728L;
+	private JButton testButton;
+
+	public ToolBarUML() {
+		testButton = new JButton("test");
+		addComponents(); 
+	} 
+	
+	public void addComponents() {
+		add(testButton);
+	}
+}
